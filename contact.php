346e87d1f50ec5466a003e31f842b30eb7e6fdab
AriYV <aridaman007@gmail.com>
2017-01-10 19:44:20 +0530

    herokudp

--- contact.php
+++ contact.php
@@ -1,35 +1,4 @@
 <?php
-$field_name = $_POST['name'];
-$field_phone = $_POST['phone'];
-$field_email = $_POST['email'];
-$field_message = $_POST['message'];
-
-$mail_to = 'aridaman007@gmail.com';
-$subject = 'Message from a site visitor '.$field_name;
-
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Phone: '.$field_phone."\n";
-$body_message .= 'Message: '.$field_message;
-
-
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
-
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
-
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'landingpage.html';
-	</script>
-<?php
-}
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to aridaman007@gmail.com');
-		window.location = 'landingpage.html';
-	</script>
-<?php
-}
+mail('aridaman007@gmail.com','Test mail','The mail function is working!');
+echo 'Mail sent!';
 ?>
